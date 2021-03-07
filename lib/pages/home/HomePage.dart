5a8078f819f6b4bc3a4af30dd23a173f82fb6952
kperez <kevin.edgard.perez@gmail.com>
2021-03-06 22:06:30 -0500

    sticky tabs

--- lib/pages/home/HomePage.dart
+++ lib/pages/home/HomePage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:quipumarket/pages/home/Widgets.dart';
-import 'package:quipumarket/theme/Dimens.dart';
 import 'package:quipumarket/theme/Styles.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,8 +10,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
 
-  int playDown = 1;
-  bool visible = true;
   late TabController _controller;
 
   @override
@@ -24,149 +21,185 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
   @override
   Widget build(BuildContext context) {
     
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    
     return DefaultTabController(
       length: 2,
-      child: Scaffold(
-        //appBar: AppBarMarket(),
-        body: SafeArea(
-          child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
+      child: SafeArea(
+        child: Scaffold(
+          body: CustomScrollView(
+            slivers: <Widget>[
               
-              final height = constraints.maxHeight;
-              final width = constraints.maxWidth;
-
-              /*return Container(
-                width : constraints.maxWidth,
-                height: constraints.maxHeight,
-                padding: horizontalScaffoldPadding(context),
-                child: Column(
-                  children: <Widget> [
-                    SizedBox(height: height * .04),
-                    ProfileWidget(height: height, width: width),
-                    SizedBox(height: height * .02),
-                    ShopButton.blue(width: width, title: "Ver catálogo",),
-                    SizedBox(height: height * .02,),
-                    AboutMarketButtons(witdth: width,)
-                    // BannerWidget(height: height*.35,),
-                  ],
+              /*SliverAppBar(
+                //pinned: true,
+                floating: false,
+                expandedHeight: 120.0,
+                centerTitle: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text('Basic Slivers'),
                 ),
-              );*/
-              return SingleChildScrollView(
-                child: Stack(
-                  children: <Widget>[
-                    Container(
-                      height: constraints.maxHeight,
-                      padding: horizontalScaffoldPadding(context),
-                      child: Column(
-                        children: <Widget> [
-                          SizedBox(height: height * .04),
-                          ProfileWidget(height: height, width: width),
-                          SizedBox(height: height * .02),
-                          ShopButton.blue(width: width, title: "Ver catálogo",),
-                          SizedBox(height: height * .03,),
-                          AboutMarketButtons(witdth: width,),
-                          SizedBox(height: height * .03,),
-                        ],
+              ),*/
+              /*SliverFixedExtentList(
+                itemExtent: 50,
+                delegate: SliverChildListDelegate([
+                  Container(color: Colors.red),
+                  Container(color: Colors.green),
+                  Container(color: Colors.blue),
+                ]),
+              ),*/
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return getMarketPresentationWidgets(width: width, height: height,)[index];
+                  },
+                  childCount: getMarketPresentationWidgets(width: width, height: height,).length,
+                ),
+              ),
+              SliverPersistentHeader(
+                pinned: true,
+                floating: true,
+                delegate: _StickyTabBarDelegate(
+                  TabBar(
+                    controller: _controller,
+                    tabs: <Widget>[
+                      Tab(
+                        text: "Novedades",
                       ),
-                    ),
-                  ], 
+                      Tab(
+                        text: "Ofertas",
+                      )
+                    ],
+                  ),
                 )
-              );
-            },
+              ),
+              SliverFillRemaining(
+                child: TabBarView(controller: _controller, children: <Widget> [
+                  Container(
+                    child: Wrap(
+                      children: [
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Wrap(
+                      children: [
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                        containerDummy(width),
+                      ],
+                    ),
+                  ),
+              ])),
+              
+            ],
           ),
         ),
-      ),
+      )
     );
+  }
+  List<Widget> getMarketPresentationWidgets({double width = 0, double height = 0}) {
+    return [
+      SizedBox(height: height * .04),
+      ProfileWidget(height: height, width: width),
+      SizedBox(height: height * .02),
+      ShopButton.blue(width: width, title: "Ver catálogo",),
+      SizedBox(height: height * .03,),
+      AboutMarketButtons(witdth: width,),
+      SizedBox(height: height * .03,),
+    ];
+  }
+
+    /**
+   return SingleChildScrollView(
+    child: Stack(
+        children: <Widget>[
+          Container(
+            height: constraints.maxHeight,
+            padding: horizontalScaffoldPadding(context),
+            child: Column(
+              children: <Widget> [
+                SizedBox(height: height * .04),
+                ProfileWidget(height: height, width: width),
+                SizedBox(height: height * .02),
+                ShopButton.blue(width: width, title: "Ver catálogo",),
+                SizedBox(height: height * .03,),
+                AboutMarketButtons(witdth: width,),
+                SizedBox(height: height * .03,),
+              ],
+            ),
+          ),
+        ], 
+      )
+    );
+   */
+}
+
+class _StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
+  const _StickyTabBarDelegate(this.tabBar);
+
+  final TabBar tabBar;
+
+  @override
+  double get minExtent => tabBar.preferredSize.height;
+
+  @override
+  double get maxExtent => tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return Container(child: tabBar);
+  }
+
+  @override
+  bool shouldRebuild(_StickyTabBarDelegate oldDelegate) {
+    return tabBar != oldDelegate.tabBar;
   }
 }
 
-Widget row( double width) {
-  return Row(
-    children: [
-      Container(
-        child: Container(
-          width: width/3,
-          height: width/3,
-          child: Container(
-            margin: EdgeInsets.all(0.5),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(1)
-            ),
-            child: Text("6"),
-          )
+Widget containerDummy(double width){
+  return Container(
+    child: Container(
+      width: width/3,
+      height: width/3,
+      child: Container(
+        margin: EdgeInsets.all(0.5),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(1)
         ),
-      ),
-      Container(
-        child: Container(
-          width: width/3,
-          height: width/3,
-          child: Container(
-            margin: EdgeInsets.all(0.5),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(1)
-            ),
-            child: Text("6"),
-          )
-        ),
-      ),
-      Container(
-        child: Container(
-          width: width/3,
-          height: width/3,
-          child: Container(
-            margin: EdgeInsets.all(0.5),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(1)
-            ),
-            child: Text("6"),
-          )
-        ),
-      ),
-    ],
+        child: Text("6"),
+      )
+    ),
   );
 }
 
-class ShopGallery extends StatefulWidget {
-  ShopGallery({this.width = 0});
-
-  final double width;
-
-  @override
-  _ShopGalleryState createState() => _ShopGalleryState();
-}
-
-class _ShopGalleryState extends State<ShopGallery> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: widget.width,
-            mainAxisExtent: widget.width,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 1,
-            mainAxisSpacing: 1),
-        itemCount: myProducts.length,
-        itemBuilder: (BuildContext ctx, index) {
-          return Container(
-            alignment: Alignment.center,
-            child: Text(myProducts[index]["name"]),
-            decoration: BoxDecoration(
-                color: Colors.amber,
-                borderRadius: BorderRadius.circular(2)),
-          );
-        }),
-    );
-  }
-}
-
-final List<Map> myProducts =
-      List.generate(100000, (index) => {"id": index, "name": "Product $index"})
-          .toList();
 
 
 
